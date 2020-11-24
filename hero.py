f4767a98aba66d8c39856b24d40663df2217eaa9
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2020-11-24 04:30:20 -0800

    SP @ is_alive

--- hero.py
+++ hero.py
@@ -44,9 +44,12 @@ class Hero:
         '''
         Calculate the total block amount from all armor blocks.
         return: total_block:Int
-        
+
         This method should run the block method on each armor in self.armors
         '''
+        total_armor = 0
+        for armor in self.armors:
+            total_armor += armor.block()
   
 
 ##############################################################
@@ -55,6 +58,38 @@ class Hero:
     def fight(self, opponent):
         winner = random.choice([self.name, opponent.name])
         print(f'{winner} has won the fight!')
+
+##############################################################
+# Is Alive Method
+##############################################################
+    def is_alive(self):
+        '''
+        Return True or False depending on whether the hero is alive or not.
+
+        TODO: Check the current_health of the hero.
+        if it is <= 0, then return False. Otherwise, they still have health
+        and are therefore alive, so return True
+        '''
+        if self.current_health > 0:
+            return True
+        else:
+            return False
+
+##############################################################
+# Take Damage
+##############################################################
+    def take_damage(self, damage):
+        '''
+        Updates self.current_health to reflect the damage minus the defense.
+
+        TODO: Create a method that updates self.current_health to the current
+        minus the the amount returned from calling self.defend(damage).
+        '''
+        self.current_health -= damage
+        # self.current_health -= self.defend(damage)
+        return self.current_health
+
+
             
 ##############################################################
 # Debugging / Testing
@@ -64,7 +99,7 @@ if __name__ == "__main__":
     # this block is executed.
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
-    # hero1.fight(hero2)
+    #hero1.fight(hero2)
 
     my_hero = Hero("Grace Hopper", 200)
     # print(my_hero.name)
@@ -75,6 +110,18 @@ if __name__ == "__main__":
     hero = Hero("Grace Hopper", 200)
     hero.add_ability(ability)
     hero.add_ability(another_ability)
-    print(hero.attack())
+    #print(hero.attack())
+
+    hero = Hero("Grace Hopper", 200)
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    #print(hero.current_health)
+
+    hero = Hero("Grace Hopper", 200)
+    hero.take_damage(150)
+    print(hero.is_alive())
+    hero.take_damage(15000)
+    print(hero.is_alive())
 
     
