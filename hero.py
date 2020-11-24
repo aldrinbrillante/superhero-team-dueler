4078e63c4e1b8c286373c089ae2466f3609c25c6
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2020-11-24 03:33:05 -0800

    hero.py CP

--- hero.py
+++ hero.py
@@ -1,21 +1,24 @@
+import random
+
 class Hero:
     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
-        # we know the name of our hero, so we assign it here
         self.name = name
-        # similarly, our starting health is passed in, just like name
         self.starting_health = starting_health
-        # when a hero is created, their current health is
-        # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
         
-
+    def fight(self, opponent):
+        winner = random.choice([self.name, opponent.name])
+        print(f'{winner} has won the fight!')
             
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    my_hero = Hero("Grace Hopper", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    
+    hero1.fight(hero2)
+
+    
