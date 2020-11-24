f6e9fa6e2746479e94a7401f183fa989fcaadbc7
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2020-11-24 02:58:57 -0800

    CP: OOP: Dogs

--- my_dogs.py
+++ my_dogs.py
@@ -2,4 +2,8 @@
 from dog import Dog # we need to specify exactly what we want
 
 my_dog = Dog("Rex", "SuperDog")
+my_other_dog = Dog("Annie", "SuperDog")
+
 my_dog.bark()
+print(my_other_dog.name)
+
